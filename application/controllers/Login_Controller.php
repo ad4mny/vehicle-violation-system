a26f02f3ed5537af7cc741f537b74ad04a1cc8ca
adamz <58021562+ad4mny@users.noreply.github.com>
2021-07-22 06:17:38 +0800

    added mvc boilerplates and templates alongside with assets files

---
+++ application/controllers/Login_Controller.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login_Controller extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('fragments/Header');
+		$this->load->view('users/Login_View');
+		$this->load->view('fragments/Footer');
+	}
+}
---
+++ application/views/fragments/Footer.php
@@ -0,0 +1,11 @@
+<!-- Footer Section -->
+<div class="d-none d-lg-block text-center py-2">
+    <small>&copy; SUVVS 2021 </small>
+</div>
+
+<!-- Script Section -->
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
