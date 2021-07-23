b683757ead4f64f1693c287b416c45f76e05ecd1
adamz <58021562+ad4mny@users.noreply.github.com>
2021-07-24 07:58:28 +0800

    updated dashboard view

--- application/views/templates/Footer.php
+++ application/views/templates/Footer.php
@@ -1,10 +1,6 @@
 </div>
 </div>
 
-<div class="d-none d-lg-block text-center py-2">
-    <small>&copy; SUVVS 2021 </small>
-</div>
-
 <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
 
--- application/views/users/Dashboard_View.php
+++ application/views/users/Dashboard_View.php
@@ -1,3 +1,48 @@
-<div class="container">
-    This is dashboard.
+<div class="container m-3 pb-5">
+    <div class="row my-3">
+        <div class="col">
+            <h1 class="display-4">Dashboard</h1>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col">
+            <h5 class=" border-bottom pb-1">Saman-On-The-GO UMP Vehicle Violation System (SUVSS)</h5>
+            <p class=" text-justify">No content yet.</p>
+        </div>
+    </div>
+    <div class="row ">
+        <div class="col">
+            <h5 class=" border-bottom pb-1">Penalty type and points deduction</h5>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-6">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Position</th>
+                        <th>Penalty Point</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Cause accident</td>
+                        <td>500</td>
+                    </tr>
+                    <tr>
+                        <td>Parking violation</td>
+                        <td>400</td>
+                    </tr>
+                    <tr>
+                        <td>No sticker displayed</td>
+                        <td>300</td>
+                    </tr>
+                    <tr>
+                        <td>Not wearing seat belt or helmet</td>
+                        <td>200</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
