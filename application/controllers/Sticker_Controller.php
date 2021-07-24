830edc224df201765639d9bffa720ba8c6301c45
adamz <58021562+ad4mny@users.noreply.github.com>
2021-07-24 09:18:39 +0800

    updated sticker controller to handle multiple pages

--- application/controllers/Sticker_Controller.php
+++ application/controllers/Sticker_Controller.php
@@ -9,20 +9,33 @@ class Sticker_Controller extends CI_Controller
         $this->load->model('Sticker_Model');
     }
 
-    public function index($page = null, $id = null)
-	{
-		$this->load->view('templates/Header');
-		$this->load->view('templates/Navigation');
-		$this->load->view('users/Sticker_List_View');
-		$this->load->view('templates/Footer');
-	}
+    public function index($page = 'sticker', $id = null)
+    {
+        $this->load->view('templates/Header');
+        $this->load->view('templates/Navigation');
+
+        if ($page === 'apply') {
+            $this->load->view('users/Application_Form');
+        } else if ($page === 'view') {
+            $data['sticker_data'] = $this->view_sticker($id);
+            $this->load->view('users/Sticker_View', $data);
+        } else if ($page === 'update') {
+            $data['sticker_data'] = $this->view_sticker($id);
+            $this->load->view('users/Sticker_View', $data);
+        } else {
+            $data['sticker_list'] = $this->view_sticker_list();
+            $this->load->view('users/Sticker_List_View', $data);
+        }
+
+        $this->load->view('templates/Footer');
+    }
 
     public function apply_sticker()
     {
         return 0;
     }
 
-    public function delete_sticker()
+    public function delete_sticker($id)
     {
         return 0;
     }
@@ -32,7 +45,7 @@ class Sticker_Controller extends CI_Controller
         return 0;
     }
 
-    public function view_sticker()
+    public function view_sticker($id)
     {
         return 0;
     }
