63e90025d0b0e73d7b9cdb0fabf70acc3cda033d
adamz <58021562+ad4mny@users.noreply.github.com>
2021-07-24 09:18:47 +0800

    added new routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,5 +9,7 @@ $route['login'] = 'Login_Controller/login';
 $route['logout'] = 'Login_Controller/logout';
 $route['dashboard'] = 'Dashboard_Controller/index';
 $route['sticker'] = 'Sticker_Controller/index';
+$route['sticker/(:any)'] = 'Sticker_Controller/index/$1';
+$route['sticker/apply/submit'] = 'Sticker_Controller/apply_sticker';
 $route['violation'] = 'Violation_Controller/index';
 $route['report'] = 'Report_Controller/index';
