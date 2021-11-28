b422040101ed943ac73fa4874f61e043bc1f66a0
adamz <58021562+ad4mny@users.noreply.github.com>
2021-11-28 20:15:44 +0800

    update index files and htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/vehicle-violation-system/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
